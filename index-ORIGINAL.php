3bb4728bb14543865b47ed3aa7f3b478fd88a878
nixxx27 <nikko.zabala@gmail.com>
2018-08-31 11:32:34 +0800

    thank you credit

--- index-ORIGINAL.php
+++ index-ORIGINAL.php
@@ -1,4 +1,8 @@
+<?php include 'check_browser.php'; ?>
 
+<?php  if (ae_detect_ie()) {  ?>
+	<h2 style="text-align: center">It seems you are using Internet Explorer which didnt support Animation. :(  <br> please use Firefox or Chrome or contact your administrator</h2>
+<?php }  ?>
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 	<head>
@@ -12,9 +16,12 @@
 		<link rel="shortcut icon" href="../favicon.ico">
 		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
 		<link rel="stylesheet" type="text/css" href="css/fontawesome-free-5.0.10/web-fonts-with-css/css/fontawesome.min.css" />
+
 		<link rel="stylesheet" type="text/css" href="css/demo.css" />
 		<link rel="stylesheet" type="text/css" href="css/animate.css" />
 		<link rel="stylesheet" type="text/css" href="css/mockup4.css" />
+		\	<link rel="stylesheet" type="text/css" href="css/style.css" />
+
 		<script src="js/modernizr.custom.js"></script>
 		
 
@@ -58,6 +65,122 @@
 					<header class="codrops-header">
 			
 						<h1 style="color:white;border:white"><p class="welcome"> </p> </h1>
+
+	<div class='wrapper'>
+  <div class='movie'>Nikko Zabala, Now Signing Off...</div>
+  <div class='movie'>THANKS TO</div>
+
+   <div class='movie'>IT Department <3 </div>
+  <div class='job'>IT Officer</div>
+  <div class='name'>Jeff Iral</div>
+  <div class='job'>IT Specialist</div>
+  <div class='name'>Kevin Sia</div>
+
+  <div class='movie'>Two E-Com Family </div>
+  <div class='job'>VP Finance & Admin</div>
+  <div class='name'>Sir Prisco Ponce</div>
+  <div class='job'>Chief Financial Officer</div>
+  <div class='name'>Mam Florence Siy</div>
+
+  <div class='job'>Assistant to the VP and CFO</div>
+  <div class='name'>Mam Jerlyn Cotoner</div>
+
+  <div class='job'>Accounting Manager</div>
+  <div class='name'>Mam Rose Anne Romasanta</div>
+
+  <div class='job'>Finance Manager</div>
+  <div class='name'>Mam Germelina Deocariza</div>
+
+  <div class='job'>General Accounting Officer</div>
+  <div class='name'>Aira Fulo</div>
+
+  <div class='job'>Treasury Supervisor</div>
+  <div class='name'>Elise Diana Dee</div>
+
+  <div class='job'>Cost Accounting Supervisor</div>
+  <div class='name'>Michelle Talamante</div>
+
+  <div class='job'>Cost Accounting Specialist</div>
+  <div class='name'>Clarish Clemente</div>
+
+  <div class='movie'>Payables Group</div>
+  <div class='job'>Payables Supervisor</div>
+  <div class='name'>Sir Jon Jon Santos</div>
+  <div class='job'>Payables Specialists</div>
+  <div class='name'>Evelyn Mendoza | Jaizle Dee Trenonce</div>
+  <div class='name'>Shieladine Tersol | Donalyn Mumar</div>
+  <div class='name'>Sherwin Balbaboco | Roel Thoy Layoso</div>
+
+  <div class='movie'>Billing Group</div>
+  <div class='job'>Billing Officer</div>
+  <div class='name'>Maan Brillantes</div>
+  
+  <div class='job'>Billing Supervisor</div>
+  <div class='name'>Corlee Larena</div>
+
+  <div class='job'>Billing Specialist</div>
+  <div class='name'>Jhen Cantiga <3</div>
+  <div class='name'>Leah Maitim</div>
+  <div class='name'>Joan Jardin</div>
+
+  <div class='movie'>Timekeeping & Payroll Group</div>
+   <div class='job'>Timekeeping & Payroll Officer</div>
+  <div class='name'>Mam Jill Regalado </div>
+
+  <div class='name'>Irish | MC | France | AC | Luis | Christian | RJ</div>
+
+  <div class='job'>Payroll Supervisor</div>
+  <div class='name'>Mam Jack Bolanos </div>
+  <div class='name'>Joel | Maria Clemencia "Cleng" | Roxanne</div>
+
+  <div class='movie'>Purchasing & Warehouse</div>
+
+  <div class='job'>Purchasing & Warehouse Manager</div>
+  <div class='name'>Mam Irene Uy </div>
+  <div class='job'>Officers </div>
+  <div class='name'>Sir Ronielo Bulan | Mam Edna Noguera</div>
+
+  <div class='job'>Warehouse Supervisor </div>
+  <div class='name'>Mam Maricel Garcia</div>
+
+  <div class='name'>Miss Dona | Merj | Joan | Justine | Hanna | Leonard | To All Warehouse Team</div>
+
+  <div class='movie'>HR Department</div>
+  <div class='job'>HR Manager</div>
+   <div class='name'>Sir Ronero Tan</div>
+
+   <div class='job'>HR Officers</div>
+   <div class='name'>Mam Malou Tungol | Sir Jayson Rocento</div>
+
+  <div class='name'>Miss Carla | Sarah | Jocelyn | AC | Jen | Alvin  | Andrew | Jubelyn</div>
+
+  <div class='job'>SkyLogistics</div>
+  <div class='job'>President</div>
+  <div class='name'>Sir Francisco Yngente</div>
+  <div class='job'>General Manager</div>
+  <div class='name'>Mam Analyn Castro</div>
+
+ <div class='movie'>AND TO ALL SKYLOGISTICS FAMILY!!! </div>
+
+  <div class='job'>SkyKitchen</div>
+  <div class='job'>President</div>
+  <div class='name'>Sir Sam Lim</div>
+
+  <div class='job'>General Manager</div>
+  <div class='name'>Sir Wesley Calfoforo</div>
+
+    <div class='job'>Operations Manager</div>
+  <div class='name'>Sir Victor Lotuaco</div>
+
+  <div class='movie'>AND TO ALL SKYKITCHEN FAMILY!!! </div>
+
+    <div class='name'>Field Servicing | Catering Operations </div>
+	<div class='name'> Training | PSD | RAMP </div>
+	<div class='name'>CARGO | MSC | CBS</div>
+    <div class='name'>GEMSD | QAD | SSD</div>
+    <div class='movie'>THANKS TO ALL ... <3</div>
+</div>
+
 		
 					</header>
 				</div>
@@ -114,7 +237,8 @@
 		<script type="text/javascript">
      $(function(){
       $(".welcome").typed({
-        strings: ['THANK YOU!', 'SKY!!!','NIKKO ZABALA','IS NOW','SIGNING','', 'OFF...','SALAMAT PO!!',''],
+        // strings: ['THANK YOU!', 'SKY!!!','NIKKO ZABALA','IS NOW','SIGNING','', 'OFF...','SALAMAT PO!!',''],
+        strings: ['THANK YOU!', 'SKY!!!',''],
         typeSpeed: 100,
         showCursor: false,
 
